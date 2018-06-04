6585244f64e458628dafbb853c7c63988b75d489
Tamas Csonka <csnktms@gmail.com>
2018-06-04 12:47:54 +0200

    feat: add CTA's, and SVG icons

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,34 @@
 import React from 'react'
 import styles from './index.module.css'
 
+import CTA from '../CTA'
+import Exchange from '../SVG/Exchange'
+import Swap from '../SVG/Swap'
+import Portfolio from '../SVG/Portfolio'
+
+const ctas = [
+  {
+      logo: <Exchange />,
+      title: 'Decentralized Exhange',
+      subTitle: 'Crypto ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'
+  },
+  {
+    logo: <Swap />,
+    title: 'Simple Atomic Swaps',
+    subTitle: 'Crypto ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'
+  },
+  {
+    logo: <Portfolio />,
+    title: 'Portfolio Manager',
+    subTitle: 'Crypto ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'
+  },
+]
+
 export default () => <div className={styles.main}>
-    <p>One stop crypto exchange solution</p>
+    <p className={styles.title}>One stop crypto exchange solution</p>
+    <div className={styles.ctas}>
+      {
+        ctas.map(cta => <CTA key={cta.title} logo={cta.logo} title={cta.title} subTitle={cta.subTitle} />)
+      }
+    </div>
 </div>
---
+++ src/components/SVG/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => <svg width="36" height="35" viewBox="0 0 36 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fillRule="evenodd" clipRule="evenodd" d="M0 3.75021C0 1.67899 1.67899 0 3.75021 0H32.2498C34.321 0 36 1.67899 36 3.75021V30.7536C36 32.8249 34.321 34.5039 32.2498 34.5039H3.75021C1.67899 34.5039 0 32.8249 0 30.7536V3.75021ZM3.75021 1.50008C2.50759 1.50008 1.50008 2.50749 1.50008 3.75021V10.7281H34.4999V3.75021C34.4999 2.50749 33.4924 1.50008 32.2498 1.50008H3.75021ZM34.4999 12.2282H11.2495V33.0038H32.2498C33.4924 33.0038 34.4999 31.9964 34.4999 30.7536V12.2282ZM9.74945 12.2282H1.50008V30.7536C1.50008 31.9964 2.50759 33.0038 3.75021 33.0038H9.74945V12.2282Z" fill="url(#paint0_linear)"/>
+<defs>
+<linearGradient id="paint0_linear" x2="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(18.75 -16.5331) scale(50.9117 48.7958) rotate(45)">
+<stop stopColor="#706EF5"/>
+<stop offset="1" stopColor="#8054F1"/>
+</linearGradient>
+</defs>
+</svg>
