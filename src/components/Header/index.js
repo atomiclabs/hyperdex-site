fd94c6f8feec92ad51d3adfb077e69eda1078e8a
Norbert Dragán <norbert.dragan@gmail.com>
2018-06-05 23:56:49 +0200

    Modified text styles to fit the designs

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,10 +8,10 @@ import Logo from '../SVG/Logo'
 
 export default () => <div className={styles.header}>
 	<Logo />
-	<p className={styles.title}>The decentralized exchange for Atomic Swaps.</p>
+	<p className={styles.title}><em>The</em> decentralized exchange for Atomic Swaps.</p>
 	<p className={styles.subTitle}>Desktop app for BarterDEX. Currently in private testing phase, official launch in July 2018.</p>
 	<Button text='Join the test' />
-	<p className={styles.text}>Available for Windows, Mac OS and Linux.</p>
+	<p className={styles.text}>Available for Windows, macOS and Linux.</p>
 	<Chart />
 	<img src={headerAppImage} alt='stf' className={styles.headerAppImage}/>
 	<div className={styles.divider}></div>
