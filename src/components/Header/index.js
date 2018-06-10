e205a66983059ecf51ea84fa59705397f4d65e81
Norbert Dragán <norbert.dragan@gmail.com>
2018-06-10 14:28:02 +0200

    Modify "Available..." content for better flow

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,7 +11,7 @@ export default () => <div className={styles.header}>
 	<p className={styles.title}><em>The</em> decentralized exchange for Atomic Swaps.</p>
 	<p className={styles.subTitle}>Desktop app for BarterDEX. Currently in private testing phase, official launch in July 2018.</p>
 	<Button text='Join the test' />
-	<p className={styles.text}>Available for Windows, macOS and Linux.</p>
+	<p className={styles.text}>Available for MacOS, Linux and Windows.</p>
 	<Chart />
 	<img src={headerAppImage} alt='stf' className={styles.headerAppImage}/>
 	<div className={styles.divider}></div>
