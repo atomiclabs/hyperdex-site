af6193033fe0695bf83b58732ae5ffcdf0016cfc
Norbert Dragán <norbert.dragan@gmail.com>
2018-06-24 17:50:02 +0200

    Update copy

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ import Logo from '../SVG/Logo'
 
 export default () => <div className={styles.header}>
 	<Logo />
-	<p className={styles.title}><em>The</em> decentralized exchange for Atomic Swaps.</p>
+	<p className={styles.title}>The Open Souce decentralized exchange for Atomic Swaps.</p>
 	<p className={styles.subTitle}>Desktop app for BarterDEX. Currently in private testing phase, official launch in July 2018.</p>
 	<Button text='Join the test' />
 	<p className={styles.text}>Available for MacOS, Linux and Windows.</p>
